614ddb5c858cf090b18aca16865033ed6d2b58c7
xushengfeng <xushengfeng_zg@163.com>
2022-04-13 13:17:48 +0800

    :sparkles:源码包链接更新

--- js/js.js
+++ js/js.js
@@ -247,8 +247,12 @@ function show_download() {
     document.querySelector("#Linux_d > div").innerHTML = Linux_d;
     var macOS_d = `<a target="_blank" href="${files_object.dmg.url}"><div class="download_b"><span>.dmg</span>dmg 安装镜像</div></a>`;
     document.querySelector("#macOS_d > div").innerHTML = macOS_d;
-    var source_d = `<a target="_blank" href="${result[0].tarball_url}"><div class="download_b"><span>.tar</span>tar 压缩源代码</div></a>
-    <a target="_blank" href="${result[0].zipball_url}"><div class="download_b"><span>.zip</span>zip 压缩源代码</div></a>`;
+    var source_d = `<a target="_blank" href="https://${
+        fasturl ? "hub.fastgit.xyz" : "github.com"
+    }/xushengfeng/eSearch/archive/refs/tags/${result[0].name}.tar.gz"><div class="download_b"><span>.tar</span>tar 压缩源代码</div></a>
+    <a target="_blank" href="https://${
+        fasturl ? "hub.fastgit.xyz" : "github.com"
+    }/xushengfeng/eSearch/archive/refs/tags/${result[0].name}.zip"><div class="download_b"><span>.zip</span>zip 压缩源代码</div></a>`;
     document.querySelector("#source_d > div").innerHTML = source_d;
 }
 // 旧版本下载
