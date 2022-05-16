cbf6c289785c557ec1f87071265d001ea99e9a42
xushengfeng <xushengfeng_zg@163.com>
2022-05-16 19:19:27 +0800

    :sparkles:识别mac.zip

--- src/js.ts
+++ src/js.ts
@@ -1,41 +1,38 @@
 // 导航栏跳转
 document.getElementById("nav").onclick = (e) => {
-    let el = e.target;
+    let el = <HTMLElement>e.target;
     if (el.dataset.id) {
         if (el.dataset.id == "eSearch") {
             window.scrollTo(0, 0);
-        }
-        else {
+        } else {
             window.scrollTo(0, document.getElementById(el.dataset.id).offsetTop - 48);
         }
     }
 };
+
 // 根据平台在首页显示下载按钮
 var userAgent = navigator.userAgent.toLowerCase();
 var platform = "Unknown";
 if (userAgent.indexOf("win") > -1) {
     platform = "Windows";
-}
-else if (userAgent.indexOf("iphone") > -1) {
+} else if (userAgent.indexOf("iphone") > -1) {
     platform = "iOS";
-}
-else if (userAgent.indexOf("mac") > -1) {
+} else if (userAgent.indexOf("mac") > -1) {
     platform = "macOS";
-}
-else if (userAgent.indexOf("linux") > -1) {
+} else if (userAgent.indexOf("linux") > -1) {
     if (userAgent.indexOf("android") > -1) {
         platform = "Android";
-    }
-    else {
+    } else {
         platform = "Linux";
     }
-}
-else {
+} else {
     platform = "Unknown";
 }
+
 var main_download = document.getElementById("main_download");
-var platform_select = document.getElementById("platform");
-function c_platform(platform) {
+var platform_select = <HTMLSelectElement>document.getElementById("platform");
+
+function c_platform(platform: string) {
     switch (platform) {
         case "Windows":
             main_download.innerHTML = `<button id=".exe">下载</button>`;
@@ -59,13 +56,17 @@ function c_platform(platform) {
             break;
     }
 }
+
 c_platform(platform);
+
 platform_select.oninput = () => {
     c_platform(platform_select.value);
 };
+
 var is_phone = window.matchMedia("(max-width: 900px)").matches;
+
 // 获取软件资源
-var result;
+var result: object;
 var files_object = {
     "-win.zip": { url: "", size: "" },
     ".exe": { url: "", size: "" },
@@ -76,6 +77,7 @@ var files_object = {
     ".dmg": { url: "", size: "" },
     "-mac.zip": { url: "", size: "" },
 };
+
 var requestOptions = {
     method: "GET",
     redirect: "follow",
@@ -83,35 +85,35 @@ var requestOptions = {
 fetch("https://api.github.com/repos/xushengfeng/eSearch/releases", { method: "GET" })
     .then((response) => response.text())
     .then((r) => {
-    result = JSON.parse(r);
-    for (let i in result[0].assets) {
-        let url = result[0].assets[i].browser_download_url;
-        let name = result[0].assets[i].name;
-        let hz = name.replace(/e-?[sS]earch.+[0-9]\.[0-9]\.[0-9]/, "");
-        files_object[hz].url = fasthub(url);
-        files_object[hz].size = (result[0].assets[i].size / 1024 / 1024).toFixed(2);
-    }
-    show_download();
-    show_log();
-})
+        result = JSON.parse(r);
+        for (let i in result[0].assets) {
+            let url = <string>result[0].assets[i].browser_download_url;
+            let name = <string>result[0].assets[i].name;
+            let hz = name.replace(/e-?[sS]earch.+[0-9]\.[0-9]\.[0-9]/, "");
+            files_object[hz].url = fasthub(url);
+            files_object[hz].size = (result[0].assets[i].size / 1024 / 1024).toFixed(2);
+        }
+        show_download();
+        show_log();
+    })
     .catch((error) => console.error("error", error));
 // 首页下载提示大小
 main_download.onmouseover = (e) => {
-    let el = e.target;
+    let el = <HTMLElement>e.target;
     var hz = el.id;
     if (el.id != "main_download")
         if (hz != "none") {
             el.title = `点击下载，共需${files_object[hz].size}MB`;
-        }
-        else {
+        } else {
             el.title = `${files_object[hz].size}`;
         }
 };
 main_download.onclick = (e) => {
-    let el = e.target;
+    let el = <HTMLElement>e.target;
     var url = fasthub(files_object[el.id].url);
     window.open(url);
 };
+
 var fasturl = true;
 document.getElementById("fastdownload").onclick = () => {
     fasturl = !fasturl;
@@ -120,8 +122,7 @@ document.getElementById("fastdownload").onclick = () => {
     }
     if (fasturl) {
         document.getElementById("fastdownload").innerText = "已使用快速下载链接";
-    }
-    else {
+    } else {
         document.getElementById("fastdownload").innerText = "已使用初始下载链接";
     }
     show_download();
@@ -130,11 +131,11 @@ document.getElementById("fastdownload").onclick = () => {
 function fasthub(url) {
     if (fasturl) {
         return url.replace("https://github.com", "https://download.fastgit.org");
-    }
-    else {
+    } else {
         return url.replace("https://download.fastgit.org", "https://github.com");
     }
 }
+
 // 下载界面添加按钮
 function show_download() {
     var Windows_d = `<a target="_blank" href="${files_object[".exe"].url}"><div class="download_b"><span>.exe</span>适用于 Windows7+</div></a>
@@ -147,15 +148,26 @@ function show_download() {
     var macOS_d = `<a target="_blank" href="${files_object[".dmg"].url}"><div class="download_b"><span>.dmg</span>适用于 macOS 10.8+</div></a>
     <a target="_blank" href="${files_object["-mac.zip"].url}"><div class="download_b"><span>.zip</span>绿色版 适用于 macOS 10.8+</div></a>`;
     document.querySelector("#macOS_d > div").innerHTML = macOS_d;
-    var source_d = `<a target="_blank" href="https://${fasturl ? "hub.fastgit.xyz" : "github.com"}/xushengfeng/eSearch/archive/refs/tags/${result[0].name}.tar.gz"><div class="download_b"><span>.tar</span>tar 压缩源代码</div></a>
-    <a target="_blank" href="https://${fasturl ? "hub.fastgit.xyz" : "github.com"}/xushengfeng/eSearch/archive/refs/tags/${result[0].name}.zip"><div class="download_b"><span>.zip</span>zip 压缩源代码</div></a>`;
+    var source_d = `<a target="_blank" href="https://${
+        fasturl ? "hub.fastgit.xyz" : "github.com"
+    }/xushengfeng/eSearch/archive/refs/tags/${
+        result[0].name
+    }.tar.gz"><div class="download_b"><span>.tar</span>tar 压缩源代码</div></a>
+    <a target="_blank" href="https://${
+        fasturl ? "hub.fastgit.xyz" : "github.com"
+    }/xushengfeng/eSearch/archive/refs/tags/${
+        result[0].name
+    }.zip"><div class="download_b"><span>.zip</span>zip 压缩源代码</div></a>`;
     document.querySelector("#source_d > div").innerHTML = source_d;
 }
 // 旧版本下载
-document.getElementById("download_old_b").onclick = () => { };
+document.getElementById("download_old_b").onclick = () => {};
 function c_other_version_link() {
-    document.querySelector("#download_old_b > a").href = `https://${fasturl ? "hub.fastgit.xyz" : "github.com"}/xushengfeng/eSearch/releases`;
+    (<HTMLLinkElement>document.querySelector("#download_old_b > a")).href = `https://${
+        fasturl ? "hub.fastgit.xyz" : "github.com"
+    }/xushengfeng/eSearch/releases`;
 }
+
 // 获取更新日志
 function show_log() {
     // markdown渲染
@@ -166,22 +178,25 @@ function show_log() {
     });
     // Remember old renderer, if overridden, or proxy to default renderer
     // 官方示例，链接新窗口打开
-    var defaultRender = md.renderer.rules.link_open ||
+    var defaultRender =
+        md.renderer.rules.link_open ||
         function (tokens, idx, options, env, self) {
             return self.renderToken(tokens, idx, options);
         };
     md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
         // If you are sure other plugins can't add `target` - drop check below
         var aIndex = tokens[idx].attrIndex("target");
+
         if (aIndex < 0) {
             tokens[idx].attrPush(["target", "_blank"]); // add new attribute
-        }
-        else {
+        } else {
             tokens[idx].attrs[aIndex][1] = "_blank"; // replace value of existing attr
         }
+
         // pass token to default renderer.
         return defaultRender(tokens, idx, options, env, self);
     };
+
     for (let i in result) {
         var li = document.createElement("li");
         var h = document.createElement("span");
@@ -194,11 +209,13 @@ function show_log() {
         document.getElementById("log").appendChild(li);
     }
 }
+
 document.onscroll = () => {
     main_an();
     ocr_an();
     search_an();
 };
+
 var animation_main = anime({
     targets: "#eSearch",
     easing: "easeInOutCubic",
@@ -206,15 +223,13 @@ var animation_main = anime({
     update: (anim) => {
         if (Math.round(anim.progress) >= 30) {
             document.getElementById("eSearch").style.pointerEvents = "none";
-        }
-        else {
+        } else {
             document.getElementById("eSearch").style.pointerEvents = "";
         }
         // 导航栏logo自动隐藏
         if (Math.round(anim.progress) >= 100) {
             document.getElementById("icon").className = "";
-        }
-        else {
+        } else {
             document.getElementById("icon").className = "icon_h";
         }
     },
@@ -226,6 +241,7 @@ function main_an() {
     scroll_percent = scroll_percent * 1.2;
     animation_main.seek(animation_main.duration * scroll_percent);
 }
+
 var animation = anime({
     targets: "#search span, #translate span",
     translateY: function () {
@@ -250,21 +266,27 @@ var tl2 = anime.timeline({
     easing: "easeInOutCubic",
     duration: 800,
 });
+
 // Add children
 if (!is_phone)
     tl2.add({
         targets: "#ocr_p > :nth-child(2)",
         width: "0",
-    }).add({
-        targets: "#ocr_p > :nth-child(3)",
-        opacity: 0,
-    }, "-=800");
+    }).add(
+        {
+            targets: "#ocr_p > :nth-child(3)",
+            opacity: 0,
+        },
+        "-=800"
+    );
+
 function ocr_an() {
     var bcr = document.getElementById("ocr_p").getBoundingClientRect();
     var scroll_percent = bcr.top / document.documentElement.clientHeight;
     scroll_percent = scroll_percent * 1.2 - 0.2;
     tl2.seek(tl2.duration * scroll_percent);
 }
+
 document.onkeydown = (e) => {
     var bcr = document.getElementById("gn_keyboard").getBoundingClientRect();
     var o = {
@@ -281,21 +303,18 @@ document.onkeydown = (e) => {
     arrow = o[e.key];
     if (document.getElementById(`key_${e.key}`)) {
         document.getElementById(`key_${e.key}`).className = "kbd_b";
-        if (bcr.top + bcr.height >= 0)
-            e.preventDefault();
+        if (bcr.top + bcr.height >= 0) e.preventDefault();
     }
     if (e.ctrlKey) {
         document.getElementById("key_ctrl").className = "kbd_b";
         d = 5;
-    }
-    else if (e.shiftKey) {
+    } else if (e.shiftKey) {
         document.getElementById("key_shift").className = "kbd_b";
         d = 10;
-    }
-    else {
+    } else {
         d = 1;
     }
-    let key_el = document.querySelector(".gn_clip_keyboard_c > :nth-child(4)");
+    let key_el = <HTMLElement>document.querySelector(".gn_clip_keyboard_c > :nth-child(4)");
     let xx = key_el.style.left || "0";
     let yy = key_el.style.top || "0";
     let x = Number(xx.replace("px", ""));
@@ -319,34 +338,34 @@ document.onkeydown = (e) => {
             break;
     }
 };
+
 document.onkeyup = (e) => {
-    if (document.getElementById(`key_${e.key}`))
-        document.getElementById(`key_${e.key}`).className = "";
-    if (e.key == "Control")
-        document.getElementById("key_ctrl").className = "";
-    if (e.key == "Shift")
-        document.getElementById("key_shift").className = "";
+    if (document.getElementById(`key_${e.key}`)) document.getElementById(`key_${e.key}`).className = "";
+    if (e.key == "Control") document.getElementById("key_ctrl").className = "";
+    if (e.key == "Shift") document.getElementById("key_shift").className = "";
 };
+
 setInterval(() => {
     var i = Math.random();
     document.getElementById("opacity_b").style.left = i * 184 + "px";
     document.getElementById("opacity_p").innerHTML = Math.round(i * 100) + "%";
-    document.querySelector(".opacity_win").style.opacity = String(i);
+    (<HTMLElement>document.querySelector(".opacity_win")).style.opacity = String(i);
 }, 1000);
+
 var back = false;
-document.querySelector(".desk").onclick = () => {
+(<HTMLElement>document.querySelector(".desk")).onclick = () => {
     back = !back;
-    let back_win = document.querySelector(".back_win");
+    let back_win = <HTMLElement>document.querySelector(".back_win");
     if (back) {
         back_win.style.left = "20px";
         back_win.style.top = "20px";
-    }
-    else {
+    } else {
         back_win.style.left = "";
         back_win.style.top = "";
     }
 };
-document.querySelector("#ding_p > :nth-child(3)").style.height =
-    document.querySelector("#ding_p > :nth-child(2)").offsetHeight - 16 + "px";
-document.querySelector(".ct_win").style.top =
-    document.querySelector("#ding_p > :nth-child(3) > :nth-child(3)").offsetTop + "px";
+
+(<HTMLElement>document.querySelector("#ding_p > :nth-child(3)")).style.height =
+    (<HTMLElement>document.querySelector("#ding_p > :nth-child(2)")).offsetHeight - 16 + "px";
+(<HTMLElement>document.querySelector(".ct_win")).style.top =
+    (<HTMLElement>document.querySelector("#ding_p > :nth-child(3) > :nth-child(3)")).offsetTop + "px";
