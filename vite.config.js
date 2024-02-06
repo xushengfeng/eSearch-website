908234d255ba10d133eab88db07b10f9cb20e853
xushengfeng <xushengfeng_zg@163.com>
2024-02-06 19:12:16 +0800

    主要 移除插件下载

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ module.exports = defineConfig({
             input: {
                 main: resolve(__dirname, "index.html"),
                 nested: resolve(__dirname, "en.html"),
-                plugin: resolve(__dirname, "plugin.html"),
                 download: resolve(__dirname, "download.html"),
                 ocr: resolve(__dirname, "ocr.html"),
             },
